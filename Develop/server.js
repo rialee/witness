23d6b6bbbf4e6970066b91fb48139f31ea64f70a
Victoria Lee <victoriacmlee@gmail.com>
2020-12-06 21:46:32 -0500

    development file structure update

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,40 @@
 // dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 // PORT setup
-const PORT = process.env.PORT || 3000;
 const app = express();
+
+// PORT
+const PORT = process.env.PORT || 3000
+
+// logger
+app.use(logger("development"));
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
 
+app.use(express.static("public"));
+
+// connection
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+    );
 
 // routing
-app.get("/", function (req, res) {
-
-    res.send("Test Connection Working!");
-
-});
+app.get("/", function (){
+    res.send("server testing")
+})
+// app.use(require("./routes/html-routes.js"))
 
 // start server and listen on PORT
 app.listen(PORT, () => {
